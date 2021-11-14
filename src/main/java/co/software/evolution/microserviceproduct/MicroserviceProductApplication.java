ea102482cef82f8ab5b1ad47b4dc927fc0ef68ec
Pedro Luis <plchavez@ucundinamarca.edu.co>
2021-11-13 20:56:22 -0500

    First Commit

---
+++ src/main/java/co/software/evolution/microserviceproduct/MicroserviceProductApplication.java
@@ -0,0 +1,13 @@
+package co.software.evolution.microserviceproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceProductApplication.class, args);
+	}
+
+}
